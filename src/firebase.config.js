9bfb1e02b58c97e5f83c34d216f099491819ce19
Alfaza Satria <alfazasatria8@gmail.com>
2020-04-16 14:55:16 +0700

    Add firebase.config.js

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyDsdanhR8ohNcCQMmUG19au23VxPQzuTHw",
+    authDomain: "fir-react-ba8f8.firebaseapp.com",
+    databaseURL: "https://fir-react-ba8f8.firebaseio.com",
+    projectId: "fir-react-ba8f8",
+    storageBucket: "fir-react-ba8f8.appspot.com",
+    messagingSenderId: "402835821722",
+    appId: "1:402835821722:web:2a6ca69b4755c373183b23"
+};
